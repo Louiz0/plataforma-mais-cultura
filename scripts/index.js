c132939e22eec50145911925dfb5d84685bda264
Luiz Felipe <luizfantini_nt@hotmail.com>
2025-06-04 13:22:12 -0300

    moving logo effect on index.html

--- scripts/index.js
+++ scripts/index.js
@@ -22,3 +22,27 @@ window.onload = () => {
         loginForm.style.display = 'none';
     }
 };
+
+const logos = document.querySelectorAll('#moving-logo');
+
+logos.forEach(logo => {
+    animateLogo(logo);
+});
+
+function animateLogo(logo) {
+    const speed = 5000 + Math.random() * 10000; // 4 a 8 seg
+    const maxX = window.innerWidth;
+    const maxY = window.innerHeight;
+
+    const newX = Math.random() * maxX;
+    const newY = Math.random() * maxY;
+
+    logo.style.transition = `transform ${speed}ms linear`;
+    logo.style.transform = `translate(${newX}px, ${newY}px)`;
+
+    setTimeout(() => animateLogo(logo), speed);
+}
+
+window.addEventListener('resize', () => {
+    logos.forEach(logo => animateLogo(logo));
+});
