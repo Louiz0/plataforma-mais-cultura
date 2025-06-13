ff51581d2aa8318c823b2fbc7c79aaaaea852d95
Luiz <luizfantini_nt@hotmail.com>
2025-06-13 09:25:38 -0300

    guest authentication and styles adjustments

--- scripts/ongs-main-page.js
+++ scripts/ongs-main-page.js
@@ -2,10 +2,19 @@ window.onload = () => {
     const postForm = document.querySelector('.new-post-frame');
     const header = document.querySelector('header');
     const pageMain = document.querySelector('.page-main');
+    const postButton = document.querySelector('#new-ong-post-button');
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const userType = urlParams.get('user');
+
+    if (userType === 'guest') {
+        postButton.style.display = 'none';
+    }
 
     window.showPostForm = function () {
         postForm.style.display = 'block';
         header.style.filter = 'blur(0.3rem)';
         pageMain.style.filter = 'blur(0.3rem)';
     }
+
 }
