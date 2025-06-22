678007596f563eb776099ffdb22e4807fcd56f94
Luiz <luizfantini_nt@hotmail.com>
2025-06-22 12:23:41 -0300

    security fixes

--- scripts/post-detail.js
+++ scripts/post-detail.js
@@ -1,7 +1,3 @@
-const SUPABASE_URL = 'https://buwppzkjtkxhieggcwuv.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1d3BwemtqdGt4aGllZ2djd3V2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyNjgwNDksImV4cCI6MjA2MDg0NDA0OX0.gFVrzw-pJ4ogws8oDOJJ86DgWo5_MVgNYkbUPizVqug'
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
 let currentPost = null;
 const editPostFrame = document.querySelector('.new-post-frame');
 const eventContainer = document.querySelector('#event-container');
